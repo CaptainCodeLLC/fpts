3be1e4fa2d67d1dee8f616f13cb66021261d8713
edv1297 <edv2@williams.edu>
2018-07-28 14:26:30 -0400

    set up react router on web router, create carousel, not really 100% functional - image resizing issues added navbar

---
+++ client/src/components/pages/Mission.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Mission extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mission</h1>
+      </div>
+    )
+  }
+}
