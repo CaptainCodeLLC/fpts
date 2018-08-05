109e3f405ec0aa718888c55aaf1fe1559042fe7e
Colombianito66 <colombianito66@gmail.com>
2018-08-05 10:08:48 -0400

    add test images

--- client/src/components/global/Carousel.js
+++ client/src/components/global/Carousel.js
@@ -6,13 +6,49 @@ import {
   CarouselCaption,
 } from 'reactstrap';
 
+import coastline from '../../images/coastline.jpg';
+import downtownNighttime from '../../images/downtownNighttime.jpg';
+import miamiSkyline from '../../images/miamiSkyline.jpg';
+import parkingGarage from '../../images/parkingGarage.jpg';
+import parkingGarage1 from '../../images/parkingGarage1.jpg';
+import rooftop from '../../images/rooftop.jpg';
+import sunrise from '../../images/sunrise.jpg';
+import sunset from '../../images/sunset.jpg';
+import traffic from '../../images/traffic.jpg';
 import wynwood from '../../images/wynwood.jpg';
 const items = [
   {
     src: wynwood,
     caption:"Wynwood Walls, Midtown"
     
-  },
+  }, {
+    src: coastline,
+    caption: "Miami Beach Coastline"
+  }, {
+    src: downtownNighttime,
+    caption: "testing"
+  }, {
+    src: miamiSkyline,
+    caption: "testing"
+  }, {
+    src: parkingGarage,
+    caption: "testing"
+  }, {
+    src: parkingGarage1,
+    caption: "testing"
+  }, {
+    src: rooftop,
+    caption: "testing"
+  }, {
+    src: sunrise,
+    caption: "testing"
+  }, {
+    src: sunset,
+    caption: "testing"
+  }, {
+    src: traffic,
+    caption: "testing"
+  }
 
 ];
 
@@ -50,7 +86,7 @@ class Example extends Component {
           key={item.src}
         >
       
-        <img className ="carousel-img"src={item.src} alt={item.altText} />
+        <img className="carousel-img" src={item.src} alt={item.altText} />
             <CarouselCaption captionHeader={item.caption} />
         </CarouselItem>   
     
