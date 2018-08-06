512a7c6c1755c0619ae4930ab2c467a34ce4173a
Eddy Varela <evare007@fiu.edu>
2018-08-06 09:39:54 -0400

    fixed positioning

--- client/src/components/global/Carousel/Slide.js
+++ client/src/components/global/Carousel/Slide.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container,Row,Col} from  'reactstrap'
+import {Container,Row,Col , Input} from  'reactstrap'
 const Slide = props => (
     <div className = "slide-container">
         <header className = "overlay"/>
@@ -7,7 +7,7 @@ const Slide = props => (
             <img src = {props.img}/>
             <Container className = "text-container">
                 <Row>
-                    <Col sm = {{size:12}} lg={{ offset:7 }}>
+                    <Col sm = {{size:12}} md = {{offset:4}} lg={{ offset:5 }}>
                             <h1>Florida Property Tax Service</h1>
                     </Col>
                 </Row>
@@ -15,7 +15,7 @@ const Slide = props => (
             </Container>
             <Container className = "text-container">
                 <Row>
-                    <Col sm = {{size:12}} lg={{ offset:7 }}>
+                    <Col sm = {{size:12}} lg={{ offset:5 }}>
                             <h2>Saving our clients millions annually</h2>
                     </Col>
                 </Row>
