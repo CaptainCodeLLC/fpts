0f84a6fbeece49e3436d17498e6ee9fbfb144843
edv1297 <edv2@williams.edu>
2018-08-08 01:42:38 -0400

    fixing problems

--- client/src/components/Mission/index.js
+++ client/src/components/Mission/index.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 import {Container} from 'reactstrap';
 import './mission.css'
-
-import { Parallax } from 'react-scroll-parallax';
-const ParallaxImage = () => (
-    <Parallax
-        className="custom-class"
-        offsetYMax={20}
-        offsetYMin={-20}
-        slowerScrollRate
-        tag="figure"
-    >
-        <img src= "https://dummyimage.com/600x400/000/fff&text=Swaggy+p"/>
-    </Parallax>)
+import Parallax from '../global/parallax/ImageParallax';
+import team from './images/TeamParallax.jpg'
 
 export default class Mission extends Component {
 
   render() {
     return (
 
-        <Container id = "mission-container">
-          <div className = "parallax-container">
+        <div id = "mission-container">
+            <Parallax src= {team}
+              reduceHeight = {2/5}
+              id = "parallax-img"
+              />
 
-          </div>
-          <h1>Our Mission</h1>
           <hr/>
           <div className = "container">
             <h2>
@@ -37,7 +28,7 @@ export default class Mission extends Component {
               Your time is valuable - let us do the work. FPTS has been in the business of reducing real estate and tangible personal property taxes since 1984, handling well over $6 billion in real estate anually in Florida. FPTS consistently produces property tax reductions at a rate that outperforms the county-wide industry average.
             </p>
             </div>
-        </Container>
+        </div>
     )
   }
 }
--- client/src/components/global/Carousel/Slide.js
+++ client/src/components/global/Carousel/Slide.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import axios from 'axios';
 import {Input, Button} from  'reactstrap'
 
@@ -49,33 +48,3 @@ export default class Slide extends React.Component {
 
   }
 }
-=======
-import {Container,Row,Col , Input} from  'reactstrap'
-const Slide = props => (
-    <div className = "slide-container">
-        <header className = "overlay"/>
-        <div className = "carousel-img">
-            <img src = {props.img}/>
-            <Container className = "text-container">
-                <Row>
-                    <Col sm = {{size:12}} md = {{offset:4}} lg={{ offset:5 }}>
-                            <h1>Florida Property Tax Service</h1>
-                    </Col>
-                </Row>
-              
-            </Container>
-            <Container className = "text-container">
-                <Row>
-                    <Col sm = {{size:12}} lg={{ offset:5 }}>
-                            <h2>Saving our clients millions annually</h2>
-                    </Col>
-                </Row>
-              
-            </Container>
-        </div>
-    
-    </div>
-)
-
-export default Slide;
->>>>>>> 512a7c6c1755c0619ae4930ab2c467a34ce4173a
